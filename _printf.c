5b9f8dff9f2952ab7cc3bd20b09acf1f2a2f4652
GG-Muniz <g.garciamuniz@outlook.com>
2025-03-28 15:50:43 -0400

    Refractoring code, eliminating switch cases and using if else to format the given characters.

--- _printf.c
+++ _printf.c
@@ -2,75 +2,45 @@
 #include <stddef.h>
 #include <stdarg.h>
 #include <unistd.h>
-
 /**
- * _printf - formats output; custom printf function with variadic arguments.
- * @format: a character string containing format specifiers.
- * Return: the number of characters printed (excluding the null byte).
- */
+ * _printf - print function that formats specific characters to output.
+ * @format: array of character to be iterated and formated.
+ * Return: formated characters.
+*/
+
 int _printf(const char *format, ...)
 {
+	/* Initialize variadic list of arguments and helper variables. */
 	va_list args;
-	int i = 0;
-	int count = 0;
-	char *str;
+	int i = 0, count = 0;
 	char c;
 
+	/* If there is no datat to format return -1.*/
 	if (format == NULL)
 		return (-1);
-
+	/* Commence variadic arguments.*/
 	va_start(args, format);
-
+	/* Format characters 'c','s','%'.*/
 	while (format && format[i])
 	{
-		if (format[i] != '%')
+		if (format[i] != '%') /* Logic explains iteration of characters. */
 		{
-			/* Regular character - write it directly */
-			write(1, &format[i], 1);
+			write(1, &format[i++], 1);
 			count++;
+			continue;
 		}
+		if (format[++i] == '\0') /* When iteration reaches null byte return -1. */
+			return (-1);
+		if (format[i] == 'c') /*Logic formats 'c'. */
+			c = va_arg(args, int), write(1, &c, 1), count++;
+		else if (format[i] == 's') /* Formating 's'. */
+			count += handle_string(va_arg(args, char *));
+		else if (format[i] == '%') /*Formatng '%'. */
+			write(1, "%", 1), count++;
 		else
-		{
-			/* Format specifier - check the next character */
-			i++;
-			/* Handle case where % is the last character */
-			if (format[i] == '\0')
-				return (-1);
-
-			switch (format[i])
-			{
-				case 'c':
-					c = va_arg(args, int);
-					write(1, &c, 1);
-					count++;
-					break;
-				case 's':
-					str = va_arg(args, char *);
-					if (str == NULL)
-						str = "(null)";
-
-					while (*str)
-					{
-						write(1, str, 1);
-						str++;
-						count++;
-					}
-					break;
-				case '%':
-					write(1, "%", 1);
-					count++;
-					break;
-				default:
-					/* Handle % followed by an unknown specifier */
-					write(1, &format[i - 1], 1);  /* Write the % character */
-					count++;
-					i--;  /* Adjust index to process current character normally next time */
-					break;
-			}
-		}
+			write(1, &format[--i], 1), count++;
 		i++;
 	}
-
-	va_end(args);
-	return (count);
+	va_end(args);/* End varaidic list of arguments.*/
+	return (count); /* Return count which hold the formated characters.*/
 }
--- main.h
+++ main.h
@@ -6,6 +6,28 @@
 #include <stdio.h>
 #include <limits.h>
 
+int handle_string(char *str);
 int _printf(const char *format, ...);
+
+/**
+ * handle_string - Helper function to print a string
+ * @str: String to print
+ * Return: Number of characters printed
+ */
+
+int handle_string(char *str)
+{
+	int count = 0;
+
+	if (str == NULL)
+		str = "(null)";
+	while (*str)
+	{
+		write(1, str++, 1);
+		count++;
+	}
+	return (count);
+}
+
 
 #endif /*MAIN_H*/
