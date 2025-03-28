95fe1947675e292c30148d7ec517faaf03fa8e42
GG-Muniz <g.garciamuniz@outlook.com>
2025-03-28 13:02:10 -0400

    Formating issue not printing desired output - refractured function. Test.

--- _printf.c
+++ _printf.c
@@ -1,8 +1,8 @@
 #include "main.h"
 #include <stddef.h>
 #include <stdarg.h>
-#include <stdio.h>
-#include <string.h>
+#include <unistd.h>
+
 /**
  * _printf - formats output; custom printf function with variadic arguments.
  * @format: a character string containing format specifiers.
@@ -10,43 +10,61 @@
  */
 int _printf(const char *format, ...)
 {
-	/* Lets declare the variables and the variadic list of arguments.*/
-	va_list args;
-	unsigned int i = 0;
-	char *str;
-	char *separator = "";
+    va_list args;
+    int i = 0;
+    int count = 0;
+    char *str;
+    char c;
 
-	if (format == NULL)
-		return (-1);
+    if (format == NULL)
+        return (-1);
 
-	/*Initializing the variadic arguments to handle their formatting.*/
-	va_start(args, format);
+    va_start(args, format);
 
-	/*Switch cases in order to move gracefully between formats.*/
-	while (format && format[i])
-	{
-		switch (format[i])
-		{
-			case 'c':
-				printf("%s%c", separator, va_arg(args, int));
-				break;
-			case 's':
-				str = va_arg(args, char *);
-				if (str == NULL)
-					str = "No string presented";
-				printf("%s%s", separator, str);
-				break;
-			case '%':
-				printf("%s%%", separator);
-				break;
-			default:
-				i++;
-				continue;
-		}
-		separator = ", ";
-		i++;
-	}
-	printf("\n"); /* New line after each case iteration.*/
-	va_end(args); /*Ending variadic arguments.*/
-	return (0); /*Function is not void - returning 0.*/
+    while (format && format[i])
+    {
+        if (format[i] != '%')
+        {
+            /* Regular character - write it directly */
+            write(1, &format[i], 1);
+            count++;
+        }
+        else
+        {
+            /* Format specifier - check the next character */
+            i++;
+            switch (format[i])
+            {
+                case 'c':
+                    c = va_arg(args, int);
+                    write(1, &c, 1);
+                    count++;
+                    break;
+                case 's':
+                    str = va_arg(args, char *);
+                    if (str == NULL)
+                        str = "(null)";
+                    while (*str)
+                    {
+                        write(1, str, 1);
+                        str++;
+                        count++;
+                    }
+                    break;
+                case '%':
+                    write(1, "%", 1);
+                    count++;
+                    break;
+                default:
+                    /* Unrecognized format - print % and the character */
+                    write(1, "%", 1);
+                    write(1, &format[i], 1);
+                    count += 2;
+                    break;
+            }
+        }
+        i++;
+    }
+    va_end(args);
+    return (count);
 }
