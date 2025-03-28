ccc051e409d63949192697ddfc57e75cb06dc493
GG-Muniz <g.garciamuniz@outlook.com>
2025-03-28 13:12:20 -0400

    Fixing issue with the null byte affecting the % formating

--- _printf.c
+++ _printf.c
@@ -6,65 +6,71 @@
 /**
  * _printf - formats output; custom printf function with variadic arguments.
  * @format: a character string containing format specifiers.
- * Return: the number of characters printed, -1 on error.
+ * Return: the number of characters printed (excluding the null byte).
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int i = 0;
-    int count = 0;
-    char *str;
-    char c;
+	va_list args;
+	int i = 0;
+	int count = 0;
+	char *str;
+	char c;
 
-    if (format == NULL)
-        return (-1);
+	if (format == NULL)
+		return (-1);
 
-    va_start(args, format);
+	va_start(args, format);
 
-    while (format && format[i])
-    {
-        if (format[i] != '%')
-        {
-            /* Regular character - write it directly */
-            write(1, &format[i], 1);
-            count++;
-        }
-        else
-        {
-            /* Format specifier - check the next character */
-            i++;
-            switch (format[i])
-            {
-                case 'c':
-                    c = va_arg(args, int);
-                    write(1, &c, 1);
-                    count++;
-                    break;
-                case 's':
-                    str = va_arg(args, char *);
-                    if (str == NULL)
-                        str = "(null)";
-                    while (*str)
-                    {
-                        write(1, str, 1);
-                        str++;
-                        count++;
-                    }
-                    break;
-                case '%':
-                    write(1, "%", 1);
-                    count++;
-                    break;
-                default:
-                    /* Unrecognized format - print % and the character */
-                    write(1, "%", 1);
-                    write(1, &format[i], 1);
-                    count += 2;
-                    break;
-            }
-        }
-        i++;
-    }
-    va_end(args);
-    return (count);
+	while (format && format[i])
+	{
+		if (format[i] != '%')
+		{
+			/* Regular character - write it directly */
+			write(1, &format[i], 1);
+			count++;
+		}
+		else
+		{
+			/* Format specifier - check the next character */
+			i++;
+			/* Handle case where % is the last character */
+			if (format[i] == '\0')
+				return (-1);
+
+			switch (format[i])
+			{
+				case 'c':
+					c = va_arg(args, int);
+					write(1, &c, 1);
+					count++;
+					break;
+				case 's':
+					str = va_arg(args, char *);
+					if (str == NULL)
+						str = "(null)";
+
+					while (*str)
+					{
+						write(1, str, 1);
+						str++;
+						count++;
+					}
+					break;
+				case '%':
+					write(1, "%", 1);
+					count++;
+					break;
+				default:
+					/* Handle % followed by an unknown specifier */
+					write(1, &format[i-1], 1);  /* Write the % character */
+					count++;
+					i--;  /* Adjust index to process current character normally next time */
+					break;
+			}
+		}
+		i++;
+	}
+
+	va_end(args);
+	return (count);
 }
